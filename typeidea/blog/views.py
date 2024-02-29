636c6714c48fc16615102ab1d8490e77a35cc472
littlehong <littlehong09@gmail.com>
2024-02-29 17:01:18 +0800

    admin 和后台更新

--- typeidea/blog/views.py
+++ typeidea/blog/views.py
@@ -1,3 +1,11 @@
+from __future__ import unicode_literals
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    return HttpResponse('白日依山尽，黄河入海流')
+
+
+def post_detail(request, post_id):
+    return HttpResponse('detail')
